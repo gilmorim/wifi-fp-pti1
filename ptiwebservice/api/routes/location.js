74085e894c54e752aec6fcd519abc34ff9f1a012
Gilberto <gilgmorim@gmail.com>
2018-09-26 17:51:27 +0100

    restructured the folders, and added the first routes designed, also templated what each route should do

---
+++ ptiwebservice/api/routes/location.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+	res.status(200).json({
+		message: "a GET on this route should query the database for the users last recorded position"
+	})
+});
+
+router.post("/", (req, res, next) => {
+	res.status(200).json({
+		message: "a POST on this route should triangulate the users location with his fingerprint, and update the database"
+	})
+});
+
+router.post("/correct", (req, res, next) => {
+	res.status(200).json({
+		message: "a POST on this route should correct the users location on the database"
+	})
+});
+
+module.exports = router;
---
+++ ptiwebservice/api/routes/users.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+	res.status(200).json({
+		message: "a GET on this route should query the database for the users list"
+	})
+});
+
+router.post("/", (req, res, next) => {
+	res.status(200).json({
+		message: "a POST on this route should add a new user"
+	})
+});
+
+router.get("/:id", (req, res, next) => {
+	res.status(200).json({
+		message: "a GET on this route should give info on the requested user id"
+	})
+});
+
+router.delete("/", (req, res, next) => {
+	res.status(200).json({
+		message: "a DELETE on this route should delete this user from the database"
+	})
+});
+module.exports = router;
